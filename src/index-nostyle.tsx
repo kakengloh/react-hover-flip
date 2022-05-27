82b53a2395bd735f8b6e42494646e74aa1d01535
kakeng <kakengloh@gmail.com>
2022-05-27 16:49:12 +0800

    Add ESLint and migrate to PNPM

--- src/index-nostyle.tsx
+++ src/index-nostyle.tsx
@@ -1,25 +1,30 @@
-import { FunctionComponent, ReactElement } from 'react'
+import { ReactElement } from 'react';
 
-interface IReactHoverFlipProps {
-    front: ReactElement
-    back: ReactElement
-    height: number
-    width: number
-    direction?: 'horizontal' | 'vertical'
+interface ReactHoverFlipProps {
+  front: ReactElement;
+  back: ReactElement;
+  height: number;
+  width: number;
+  direction?: 'horizontal' | 'vertical';
 }
 
-const ReactHoverFlip: FunctionComponent<IReactHoverFlipProps>
-    = ({ front, back, height, width, direction = "horizontal" }: IReactHoverFlipProps) => {
-    return (
-        <>
-            <div className="rhf-container" style={{ height, width }}>
-                <div className={`rhf-inner-container rhf-${direction}`}>
-                    <div className="rhf-front">{front}</div>
-                    <div className={`rhf-back rhf-${direction}`}>{back}</div>
-                </div>
-            </div>
-        </>
-    )
-}
+const ReactHoverFlip = ({
+  front,
+  back,
+  height,
+  width,
+  direction = 'horizontal',
+}: ReactHoverFlipProps) => {
+  return (
+    <>
+      <div className="rhf-container" style={{ height, width }}>
+        <div className={`rhf-inner-container rhf-${direction}`}>
+          <div className="rhf-front">{front}</div>
+          <div className={`rhf-back rhf-${direction}`}>{back}</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ReactHoverFlip
+export default ReactHoverFlip;
